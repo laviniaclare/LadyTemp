f7afdd485f7021ce089e7238c89942c6346a9f7e
Lavinia Karl <laviniaclare@gmail.com>
2014-09-27 19:38:13 -0700

    there's a graph!  It looks alright

--- graphs.js
+++ graphs.js
@@ -1,39 +1,23 @@
 $(function() {
+		var xy=[[0,98],[1,98.3],[2,98],[3,98.6],[5,98],[6,98],[8,98.5],[10,98.5],[12,98],[13,98.9],[14,98.2]];
 
-		function generate(offset, amplitude) {
+		var d5 = xy;
+		// for (var i = 0; i < 14; i += 0.5) {
+		// 	d5.push([i, Math.sqrt(i)]);
+		// }
 
-			var res = [];
-			var start = 0, end = 10;
-
-			for (var i = 0; i <= 50; ++i) {
-				var x = start + i / 50 * (end - start);
-				res.push([x, amplitude * Math.sin(x + offset)]);
-			}
-
-			return res;
-		}
-
-		var data = [
-			{ data: generate(2, 1.8), points: { symbol: "circle" } },
-			{ data: generate(3, 1.5), points: { symbol: "square" } },
-			{ data: generate(4, 0.9), points: { symbol: "diamond" } },
-			{ data: generate(6, 1.4), points: { symbol: "triangle" } },
-			{ data: generate(7, 1.1), points: { symbol: "cross" } }
-		];
-
-		$.plot("#placeholder", data, {
-			series: {
-				points: {
-					show: true,
-					radius: 3
-				}
-			},
-			grid: {
-				hoverable: true
-			}
-		});
-
-		// Add the Flot version string to the footer
+		$.plot("#placeholder", [{
+		// 	data: d2,
+		// 	bars: { show: true }
+		// },{
+		// 	data: d3,
+		// 	points: { show: true }
+		// },{
+			data: d5,
+			lines: { show: true },
+			points: { show: true, radius: 5},
+			grid: {hoverable: true}
+		}]);
 
 		$("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
 	});
